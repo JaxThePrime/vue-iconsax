e548715f4a54fcc5e2cf4731e14d82f91192d4b7
“emmoghimi” <emmoghimi@gmail.com>
2022-11-12 16:05:44 +0330

    Update version

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/components/index.ts'),
-      name: 'MyLib',
+      name: 'Vue iconsax library',
       fileName: (format) => `vsx.${format}.js`,
     },
     rollupOptions: {
