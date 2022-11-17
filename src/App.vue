bfb0314bbff5f34e83a4544f45e913abcdf507b2
emmoghimi <emmoghimi@gmail.com>
2022-11-17 20:54:34 +0330

    Fix dynamic imports

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
-    <vsx-icon iconName="Wallet" color="blue" type="bold" />
+    <vsx-icon iconName="Aave" color="blue" type="linear" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import { vsxIcon } from "./components/";
+import { Wallet } from "./components/";
 export default defineComponent({
   components: {
     vsxIcon,
+    Wallet,
   },
 });
 </script>
